ca5c94790ac1d6db849441a55443aaeb1394ee3d
Javier Barrera <js.barrerat1@uniandes.edu.co>
2024-04-12 00:10:57 -0500

    first commit

---
+++ main.ts
@@ -0,0 +1,30 @@
+import { series } from "./data.js";
+
+const seriesTable = document.getElementById('series-table')!;
+const seasonsAverage = document.getElementById('seasons-average')!;
+
+function calculateSeasonsAverage(): number {
+  const totalSeasons = series.reduce((acc, serie) => acc + serie.seasons, 0);
+  return totalSeasons / series.length;
+}
+
+function renderSeriesTable(): void {
+  series.forEach(serie => {
+    const row = document.createElement('tr');
+    row.setAttribute('id', 'serie-row');
+    row.innerHTML = `
+      <td><b>${serie.id}</b></td>
+      <td>${serie.name}</td>
+      <td>${serie.channel}</td>
+      <td>${serie.seasons}</td>
+    `;
+    seriesTable.appendChild(row);
+  });
+  const averageSeasons: number = calculateSeasonsAverage();
+  seasonsAverage.innerHTML = `${averageSeasons}`;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Ok');
+  renderSeriesTable();
+});
---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+import { series } from "./data.js";
+const seriesTable = document.getElementById('series-table');
+const seasonsAverage = document.getElementById('seasons-average');
+function calculateSeasonsAverage() {
+    const totalSeasons = series.reduce((acc, serie) => acc + serie.seasons, 0);
+    return totalSeasons / series.length;
+}
+function renderSeriesTable() {
+    series.forEach(serie => {
+        const row = document.createElement('tr');
+        row.setAttribute('id', 'serie-row');
+        row.innerHTML = `
+      <td><b>${serie.id}</b></td>
+      <td>${serie.name}</td>
+      <td>${serie.channel}</td>
+      <td>${serie.seasons}</td>
+    `;
+        seriesTable.appendChild(row);
+    });
+    const averageSeasons = calculateSeasonsAverage();
+    seasonsAverage.innerHTML = `${averageSeasons}`;
+}
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Ok');
+    renderSeriesTable();
+});
